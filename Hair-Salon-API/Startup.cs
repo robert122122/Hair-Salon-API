006fb1adc3ac7e00be89a1febe5ba07e5b14a68e
Robert Padurariu <robert.padurariu@fortech.ro>
2022-02-17 17:06:33 +0200

    Setting up the project with project references, database connection, dependencies and packagies

---
+++ Hair-Salon-API/Startup.cs
@@ -0,0 +1,108 @@
+﻿using Hair_Salon_API.DAL.Models;
+using Hair_Salon_API.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Hair_Salon_API.Services.Implementations;
+using Hair_Salon_API.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+
+namespace Hair_Salon_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("HairdresserConnectionString");
+
+            services.AddDbContext<HairdresserContext>(
+                options =>
+                    options.UseSqlServer(connectionString));
+
+
+            Assembly[] assemblies = new Assembly[]
+            {
+                Assembly.Load("Hair-Salon-API.Services"),
+                Assembly.Load("Hair-Salon-API")
+            };
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IAddressService, AddressService>();
+            services.AddScoped<ISalonService, SalonService>();
+
+            services.AddAutoMapper(assemblies);
+
+            services.AddCors();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cinema", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \n 
+                      Enter 'Bearer' [space] and then your token in the text input below. \n
+                      Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cinema v1"));
+            }
+
+            app.UseCors(options => options.AllowAnyMethod()
+                                          .AllowAnyHeader()
+                                          .AllowCredentials()
+                                          .SetIsOriginAllowed(origin => true));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
